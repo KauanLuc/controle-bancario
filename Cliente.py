50b99f82e7599bd3b598b06d20323376f2d58880
Kauan Figueiredo Lucena <kauanfigueiredolucena2016@gmail.com>
2023-04-17 20:53:08 -0300

    Adiciona o cliente a um banco de dados SQL.

--- Cliente.py
+++ Cliente.py
@@ -1,11 +1,22 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
 class Cliente:
     def __init__(self, nome, telefone, cpf, lista):
         self.nome = nome
         self.telefone = telefone
         self.cpf = cpf
-# lista adiciona o cliente no "banco de dados"
+        # criando um dataframe
         dicionario_clientes = {}
         dicionario_clientes['Nome'] = f'{self.nome}'
         dicionario_clientes['Telefone'] = f'{self.telefone}'
         dicionario_clientes['CPF'] = f'{self.cpf}'
         lista.append(dicionario_clientes)
+        database_clientes = pd.DataFrame(lista)
+        # Conectando ao banco de dados SQLite
+        engine = create_engine('sqlite:///Cliente.db')
+        # Armazenando o DataFrame como uma tabela SQL
+        database_clientes.to_sql(
+            'tabela_clientes', engine,
+            if_exists='replace', index=False
+        )
